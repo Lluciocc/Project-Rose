40243ccea9587e709f1b381219d45cf760b14c4e
lucaeas <zemarshzz@gmail.com>
2024-01-16 22:15:19 +0100

    Ajout du système de bdd JSON

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+const fs = require("fs")
 
+// Ici, nous utiliserons le fichier db.json en termes de base de données
+function readDb(dbName ="db.json"){
+    const data = fs.readFileSync(dbName)
+    return JSON.parse(data)
+}
 
-// test du json mais sans json askip mdrrrr
-
-function handleSubmit(event) {
-    event.preventDefault();
-  
-    const data = new FormData(event.target);
-  
-    const value = data.get('email');
-  
-    console.log({ value });
-  }
-  
-  const form = document.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
+function writeDb(obj, dbName="db.json"){
+    if (!obj) {
+        return console.log("Impossible de n'insérer aucun objet. Veuillez le spécifier.")
+    }
+    try {
+        fs.writeFileSync(dbName,JSON.stringify(obj))
+        return console.log("Objet inséré dans la base de données.")
+    } catch(err){
+        return console.log("Impossible d'insérer l'objet dans la base de données.")
+    }
+}
+=
