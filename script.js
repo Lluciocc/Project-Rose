e0aa0146df53d686ef3fdc8c77c600203574bef3
lluciocc <llucio.cc00@gmail.com>
2024-01-16 19:16:54 +0100

    -Base du HTML -Test du js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    const value = data.get('email');
+  
+    console.log({ value });
+  }
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
