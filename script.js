a591f9f14e60e248d05723fe6c4212e75f2beb8c
lucaeas <zemarshzz@gmail.com>
2024-01-16 23:40:07 +0100

    Essai d'implémentations HTML/CSS (Jules)

--- script.js
+++ script.js
@@ -6,15 +6,33 @@ function readDb(dbName ="db.json"){
     return JSON.parse(data)
 }
 
+// On doit changer la fonction write parce que la db est remplacée
 function writeDb(obj, dbName="db.json"){
     if (!obj) {
         return console.log("Impossible de n'insérer aucun objet. Veuillez le spécifier.")
     }
     try {
-        fs.writeFileSync(dbName,JSON.stringify(obj))
+        fs.writeFileSync(dbName,JSON.stringify(obj,null,2),"utf-8")
         return console.log("Objet inséré dans la base de données.")
     } catch(err){
         return console.log("Impossible d'insérer l'objet dans la base de données.")
     }
 }
-=
+
+// Tests
+const lucas1 = {
+    nom : "Froehlinger",
+    prenom : "Lucas",
+    classe: "T8"
+}
+const lucas2 = {
+    nom : "Ciarcello",
+    prenom : "Lucas",
+    classe: "2-2"
+}
+
+writeDb(lucas1)
+writeDb(lucas2)
+console.log(readDb())
+
+module.exports = {readDb,writeDb}
