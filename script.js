6fbb5f9aacf7e63cd115dc274bea31528f7fefc1
Snogy <juxy0.contact@gmail.com>
2024-01-19 19:58:53 +0100

    Add tabs for the orders (not finished)

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//plus rien ?
+//Page HOME
+
+function showOrder() {
+    var order = document.getElementById("order-tab").style.display = "flex";
+}
+
+function hideOrder() {
+    var order = document.getElementById("order-tab").style.display = "none";
+}
+
+function showDelivery() {
+    var delivery = document.getElementById("delivery-tab").style.display = "flex";
+}
+
+function hideDelivery() {
+    var delivery = document.getElementById("delivery-tab").style.display = "none";
+}
