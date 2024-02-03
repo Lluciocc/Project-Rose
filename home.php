38eb7c08f7c7fa8b161998d74e1e36f41302ba67
lucaeas <zemarshzz@gmail.com>
2024-02-03 13:03:09 +0100

    try to connect rn

--- home.php
+++ home.php
@@ -5,6 +5,35 @@ if (!isset($_SESSION["mdp"])){
     <script>window.location.href = "https://spcrose.fr/login"</script>
 <?php
 }
+
+$hostname = $IONOS_DB_HOST;
+$username = "$IONOS_DB_USERNAME";
+$password = "$IONOS_DB_PASSWORD";
+$db = $IONOS_DB_NAME;
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+if(isset($_POST['command'])) {
+  $prenom=$_POST['prenom'];
+  $nom=$_POST['nom'];
+  $roses=$_POST['roses'];
+  $horaires=$_POST['horaires'];
+  $salle=$_POST['salle'];
+
+  $query = "INSERT INTO commandes (nom,prenom,horaire,salle,roses)
+  VALUES ('$nom', '$prenom', '$horaires','$salle','$roses')";
+
+    if (!mysqli_query($dbconnect, $query)) {
+        die('An error occurred. Your review has not been submitted.');
+    } else {
+      echo "Thanks for your review.";
+    }
+
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -37,7 +66,7 @@ if (!isset($_SESSION["mdp"])){
     <div id="order-tab">
         <p>Prendre une commande</p>
         <input class="icon-cross" type="image" src="Images/Icon/icon-cross.png" alt="Icone croix" onclick="hideOrder()">
-        <form id="form-order">
+        <form method="post" name="command" id="form-order">
             <input type="text" id="prenom" placeholder="Prénom">
             <input type="text" id="nom" placeholder="Nom">
             <input type="text" id="roses" placeholder="Nombre de roses">
