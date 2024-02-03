ad37cd97491a77d63464e712a67c44b08ed4857c
lucaeas <zemarshzz@gmail.com>
2024-02-03 14:13:38 +0100

    Update takeorder.php

--- takeorder.php
+++ takeorder.php
@@ -5,26 +5,3 @@ $password = "$IONOS_DB_PASSWORD";
 $db = $IONOS_DB_NAME;
 
 $dbconnect=mysqli_connect("localhost",$username,$password,$db,3306);
-
-if ($dbconnect->connect_error) {
-    die("Database connection failed: " . $dbconnect->connect_error);
-}
-
-if(isset($_POST['command'])) {
-    $prenom=$_POST['prenom'];
-    $nom=$_POST['nom'];
-    $roses=$_POST['roses'];
-    $horaires=$_POST['horaires'];
-    $salle=$_POST['salle'];
-
-    $sql = "INSERT INTO commandes (nom,prenom,horaire,salle,roses)
-    VALUES ('$nom', '$prenom', '$horaires','$salle','$roses')";
-
-    if ($conn->query($sql) === TRUE) {
-            echo "Records updated: ".$student_id."-".$name."-".$age."-".$gender;
-        } else {
-            echo "Error: ".$sql."<br>".$conn->error;
-        }
-
-    $conn->close();
-}
