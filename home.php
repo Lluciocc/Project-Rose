b63b024f97ce3f7dfe44ef8994b7cd0b7e19fdff
lucaeas <zemarshzz@gmail.com>
2024-02-03 16:34:10 +0100

    Update home.php

--- home.php
+++ home.php
@@ -67,3 +67,24 @@ if (!isset($_SESSION["mdp"])){
     </footer>
 </body>
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $host_name = 'db5015066517.hosting-data.io';
+    $database = 'dbs12511800';
+    $user_name = 'dbu72595';
+    $password = 'spcWeLoveRosesSkibidi57';
+
+    $link = new mysqli($host_name, $user_name, $password, $database,3306);
+
+    $prenom=$_POST['prenom'];  
+    $nom=$_POST['nom'];
+    $roses=$_POST['roses'];
+    $horaire=$_POST['horaire']; 
+    $salle=$_POST['salle'];
+
+    $result = $conn->query("INSERT INTO commandes (nom,prenom,horaire,salle,roses) VALUES ('$nom', '$prenom', '$horaire','$salle','$roses')");
+
+    
+    $conn->close();
+    }
+?>
