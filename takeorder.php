b50065c0e4af1c167259f7987e544bba0793c8e2
lucaeas <zemarshzz@gmail.com>
2024-02-03 14:26:42 +0100

    Update takeorder.php

--- takeorder.php
+++ takeorder.php
@@ -2,6 +2,29 @@
 
 $username = "$IONOS_DB_USERNAME";
 $password = "$IONOS_DB_PASSWORD";
-$db = $IONOS_DB_NAME;
 
-$dbconnect=mysqli_connect("localhost",$username,$password,$db,3306);
+$dbconnect=mysqli_connect("localhost",$username,$password,"dbs12545785",3306);
+
+if ($dbconnect->connect_error) {
+    die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+if(isset($_POST['command'])) {
+    $prenom=$_POST['prenom'];
+    $nom=$_POST['nom'];
+    $roses=$_POST['roses'];
+    $horaires=$_POST['horaires'];
+    $salle=$_POST['salle'];
+
+    $sql = "INSERT INTO commandes (nom,prenom,horaire,salle,roses)
+    VALUES ('$nom', '$prenom', '$horaires','$salle','$roses')";
+
+    if ($conn->query($sql) === TRUE) {
+            echo "Records updated: ";
+        } else {
+            echo "Error: ".$sql."<br>".$conn->error;
+        }
+
+    $conn->close();
+}?>
+<script>window.location.href = "https://spcrose.fr/home"</script>
