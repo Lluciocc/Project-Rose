99a541bcba43a49bbc16747cb73f929a5490b5ba
lucaeas <zemarshzz@gmail.com>
2024-02-03 13:41:24 +0100

    Update home.php

--- home.php
+++ home.php
@@ -5,6 +5,34 @@ if (!isset($_SESSION["mdp"])){
     <script>window.location.href = "https://spcrose.fr/login"</script>
 <?php
 }
+
+$hostname = $IONOS_DB_HOST;
+$username = "$IONOS_DB_USERNAME";
+$password = "$IONOS_DB_PASSWORD";
+$db = $IONOS_DB_NAME;
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+if(isset($_POST['command'])) {
+  $prenom=$_POST['prenom'];
+  $nom=$_POST['nom'];
+  $roses=$_POST['roses'];
+  $horaires=$_POST['horaires'];
+  $salle=$_POST['salle'];
+
+  $query = "INSERT INTO commandes (nom,prenom,horaire,salle,roses)
+  VALUES ('$nom', '$prenom', '$horaires','$salle','$roses')";
+
+    if (!mysqli_query($dbconnect, $query)) {
+        die('An error occurred. Your review has not been submitted.');
+    } else {
+      echo "Thanks for your review.";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
