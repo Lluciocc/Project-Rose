085d516e65d9a18706956878111889bef25f90f9
lucaeas <zemarshzz@gmail.com>
2024-02-06 18:55:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function hideOrder() {
 
 function showDelivery() {
     var delivery = document.getElementById("delivery-tab").style.display = "flex";
+    var subtab = document.getElementById("delivery-subtab").style.display = "flex";
     var send = document.getElementById("delivery-send").style.display = "flex"
 }
 
 function hideDelivery() {
     var delivery = document.getElementById("delivery-tab").style.display = "none";
+    var subtab = document.getElementById("delivery-subtab").style.display = "none";
     var send = document.getElementById("delivery-send").style.display = "none"
 }
