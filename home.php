aa2bf74f5c847762b9473f6f533e7f3d80d1cfab
lucaeas <zemarshzz@gmail.com>
2024-02-06 19:50:56 +0100

    Update home.php

--- home.php
+++ home.php
@@ -100,7 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $id=$_POST['send'];
     settype($id,"int");
-    $sql = "DELETE FROM commandes WHERE id = $id; UPDATE commandes SET roses = roses + (SELECT roses FROM commandes WHERE id = $id) WHERE id = 63329;";
+    $sql = "UPDATE commandes SET nom = '', prenom = '', horaire = '00:00:00', salle='0' WHERE id = $id";
 
     if ($link->multi_query($sql) === TRUE) {
         echo "Commande livrée !";
